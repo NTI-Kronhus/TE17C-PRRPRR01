3ff0e52b86d0e0af6c246e76bff18b9c130c3eb6
deltadirac <tomas.weis@gbg.nti.se>
2018-10-29 16:48:38 +0100

    test v44

---
+++ v44/genomgång/arrayer.java
@@ -0,0 +1,19 @@
+package genomgång;
+
+public class arrayer {
+	
+	public static void main(String[] args) {
+		
+		
+		// SKapa en array med siffrorna 1 2 3 4 5
+		int[] arr = {1,2,3,4,5};
+
+		
+	
+		
+		
+		
+		
+	}
+
+}
