37d825e1b594a0ead4881e062311db403715ab23
deltadirac <tomas.weis@gbg.nti.se>
2018-11-06 10:52:59 +0100

    Repetition -  lektion 1
    repeterade datatyper och utskrifter med och utan formatering

---
+++ v45/Repetition.java
@@ -0,0 +1,21 @@
+public class Repetition {
+	
+	public static void main(String[] args) {
+		
+		int heltal2; // initiera variabel
+		heltal2 = 2; // tildela variabeln ett värde
+		int heltal = 1; // deklarerar 
+		double decimaltal = 1.123456789;
+		String ord = "Tomas ";
+		String ord2 = "Weis"; // "+" kallas att konkatinera
+		char bokstav = 'c';
+		
+		// System.out.print("Tomas hatade programmering en gång i tiden \n");
+		
+		System.out.printf("Första talet är %d, andra talet är %.2f%n", heltal2, decimaltal);
+		System.out.print("Första talet är " + heltal2 + "\n" + ord + ord2);
+		
+		
+	}
+
+}
