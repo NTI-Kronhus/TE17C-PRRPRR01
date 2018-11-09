e94adea6a3f2c4061ca84f0e69f8eafca69a4f6c
deltadirac <tomas.weis@gbg.nti.se>
2018-11-09 16:33:52 +0100

    Fredagens genomgång

---
+++ v45/RepetitionFredag.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class RepetitionFredag {
+
+	public static void main(String[] args) {
+
+		// initierar variabler
+		int tal1 = 0;
+		int summa = 0;
+		double average = 0;
+
+		Scanner input = new Scanner(System.in);
+
+		// skriver ut ett meddelande
+		System.out.print("Please type 10 numbers: ");
+
+		// summerar 10 tal som matats in från konsolen
+		for (int i = 1; i <= 10; i++) {
+			
+			tal1 = input.nextInt(); // läser in ett heltal från konsolen
+
+			// villkor som testar om talet är negativt och utför utefter det
+			if (tal1 < 0) {
+				System.out.println("Your number in negative. Please type again! ");
+				i--;
+			}
+
+			// SKRIV IN ETT VILLKOR SÅ ATT TAL > 20 INTE SKA SKRIVAS UT
+
+			// om talet är positivt så utför detta istället
+			else {
+				summa += tal1;
+			}
+		}
+
+		
+		// while-loopen nedan gör samma som for-loopen ovan
+		int antalLoopar = 1;
+
+		while (antalLoopar <= 10) {
+			// tal1 += input.nextInt(); (för att slippa upprepa inmatning)
+			antalLoopar++;
+		}
+
+		average = (double) summa / 10; // (double) gör om int till double
+
+		System.out.println(summa);
+		System.out.println(average);
+
+	}
+}
--- v45/RepetitionTisdag.java
+++ v45/RepetitionTisdag.java
@@ -1,4 +1,4 @@
-public class Repetition {
+public class RepetitionTisdag {
 	
 	public static void main(String[] args) {
 		
@@ -10,12 +10,11 @@ public class Repetition {
 		String ord2 = "Weis"; // "+" kallas att konkatinera
 		char bokstav = 'c';
 		
-		// System.out.print("Tomas hatade programmering en gång i tiden \n");
+		System.out.print("Tomas hatade programmering en gång i tiden \n");
 		
-		System.out.printf("Första talet är %d, andra talet är %.2f%n", heltal2, decimaltal);
-		System.out.print("Första talet är " + heltal2 + "\n" + ord + ord2);
-		
-		
+		System.out.printf("Första talet är %d, andra talet är %.4f%n", heltal2, decimaltal);
+		System.out.print("Första talet är " + decimaltal + "\n" + ord + ord2);
+
 	}
 
 }
