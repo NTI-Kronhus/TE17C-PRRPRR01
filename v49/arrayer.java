a9d63108c34b03388abb74bc7f7383dd997d380f
deltadirac <tomas.weis@gbg.nti.se>
2018-12-10 13:52:58 +0100

    bubble sort

--- v49/arrayer.java
+++ v49/arrayer.java
@@ -1,61 +1,53 @@
 import java.util.Scanner;
 
 public class arrayer {
-	
+
 	public static void main(String[] args) {
 
-	Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 
-	System.out.println("Hur många tal vill du skriva in?");
-	
-	// Skapar en array med 12 heltal
-	int[] olikaTal = {1, 5, 10, 47, 63, 2, -7, 7, 4, 9, 4, 6};
-	
-	// Skapar en array av storlek som bestäms av användaren från konsolen
-	int[] olikaTal2 = new int[input.nextInt()];
-	
-	// Initierar en räknare som används senare
-	int counter = 0;
-	
-	// Man kan skapa en array av decimaltal eller strängar också
-	double[] decimalTal = {1.0, 2.2, 3.5};
-	String[] namn = {"Tomas", "Ludvig", "Isak", "Dilan", "Ahmed"};
+		System.out.println("Hur många tal vill du skriva in?");
 
-	// Informerar användaren om hur många heltal man ombeds skriva
-	System.out.printf("Skriv %d heltal!\n", olikaTal2.length);
+		// Skapar en array med 12 heltal
+		int[] olikaTal = { 1, 5, 10, 47, 63, 2, -7, 7, 4, 9, 4, 6 };
 
-	// Låter användaren skriva in lika många heltal som antalet luckor i arrayen
-	for (int i = 0; i < olikaTal2.length; i++) {
-		
-		olikaTal2[i] = input.nextInt();
-	}
+		// Skapar en array av storlek som bestäms av användaren från konsolen
+		int[] olikaTal2 = new int[input.nextInt()];
 
-	// Skriver ut alla tal som matats in på en rad
-	for (int i = olikaTal2.length - 1; i >= 0; i--) {
-		
-//		System.out.print(olikaTal2[i] + ", ");
-	}
+		// Initierar en räknare som används senare
+		int counter = 0;
 
-	// Kollar hur många gånger talet "4" förekommer i int arrayen
-	for (int i = 0; i < olikaTal2.length; i++) {
-		
-		if (olikaTal2[i] == 4){
-			
-			counter++;
+		// Man kan skapa en array av decimaltal eller strängar också
+		double[] decimalTal = { 1.0, 2.2, 3.5 };
+		String[] namn = { "Tomas", "Ludvig", "Isak", "Dilan", "Ahmed" };
+
+		// Informerar användaren om hur många heltal man ombeds skriva
+		System.out.printf("Skriv %d heltal!\n", olikaTal2.length);
+
+		// Låter användaren skriva in lika många heltal som antalet luckor i arrayen
+		for (int i = 0; i < olikaTal2.length; i++) {
+
+			olikaTal2[i] = input.nextInt();
 		}
+
+		// Skriver ut alla tal som matats in på en rad
+		for (int i = olikaTal2.length - 1; i >= 0; i--) {
+
+//		System.out.print(olikaTal2[i] + ", ");
+		}
+
+		// Kollar hur många gånger talet "4" förekommer i int arrayen
+		for (int i = 0; i < olikaTal2.length; i++) {
+
+			if (olikaTal2[i] == 4) {
+//			counter++;
+			}
+		}
+
+		// Skriver ut antal 4or i arrayen
+		System.out.println(counter);
+
+		input.close();
 	}
-	
-	// Skriver ut antal 4or i arrayen
-	System.out.println(counter);
-	
-	input.close();
-}
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
---
+++ v50/BubbleSort.java
@@ -0,0 +1,75 @@
+public class BubbleSort {
+
+    public static void main(String[] args) {
+
+        // skapar en int array av storleken 20
+        int[] randomNumbers = new int[20];
+
+        for (int i = 0; i < randomNumbers.length; i++) {
+
+            // fyller arrayen med 20 slumpmässiga heltal mellan 0 och 20
+            randomNumbers[i] = (int) (Math.random() * 20) + 1;
+        }
+
+        // skriver ut arrayen
+        printIntArray(randomNumbers);
+
+        // anroper bubblesort-metoden och skriver ut antal iterationer
+        System.out.println("It took " + bubbleSort(randomNumbers) + " iterations to sort the int array using" +
+                " bubble sort algorithm.\n");
+
+        // skriver ut en sorterad array
+        printIntArray(randomNumbers);
+    }
+
+    /**
+     * Metoden sorterar heltal i storleksordning med hjälp av bubble sort algoritmen
+     * @param randomNumbers
+     * @return counter
+     */
+
+    static int bubbleSort(int[] randomNumbers) {
+
+        // initierar nödvändiga variabler
+        boolean swapped = true;
+        int counter = 0;
+
+        // loopen körs tills hela arrayen kollas igenom utan att några tal byter plats med varandra
+        do {
+
+            swapped = false;
+
+            //loopar igenom hela arrayen 1 gång
+            for (int i = 0; i < randomNumbers.length - 1; i++) {
+
+                // jämför två tal bredvid varandra och byter plats på dem om det högra talet är större än det vänstra
+                if (randomNumbers[i] > randomNumbers[i + 1]) {
+                    int temp = randomNumbers[i + 1];
+                    randomNumbers[i + 1] = randomNumbers[i];
+                    randomNumbers[i] = temp;
+                    swapped = true; // har åtminstone ett byte skett under körningen så ändras swapped värde till true
+                }
+                // håller koll på antalet iterationer
+                counter++;
+            }
+
+        } while (swapped); // do-while loopen körs alltid minst en gång
+
+        return counter; // returnerar antalet iterationer
+    }
+
+    /**
+     * Metoden skriver ut en int array, sorterad eller osorterad
+     * @param randomNumbers
+     */
+    static void printIntArray(int[] randomNumbers) {
+
+        for (int j :
+                randomNumbers) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+        System.out.println();
+    }
+}
+
